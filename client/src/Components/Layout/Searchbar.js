922feceb81753b45b6091c4f9cfd45119944473d
Ryan Jalufka <ryanjalufka@gmail.com>
2019-02-10 13:04:52 -0600

    adding files before branching

--- client/src/Components/Layout/Searchbar.js
+++ client/src/Components/Layout/Searchbar.js
@@ -1,53 +1,56 @@
 import React, { Component } from "react";
 import { Input } from "antd";
+import StockCard from "../StockCard";
 import API_KEY from "../../Utils/keys";
 const alpha = require("alphavantage")({ key: API_KEY });
 
 const Search = Input.Search;
+
+//maybe just put this stuff in global state?? idk fuck
 
 class SearchBar extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      searchTerm: "",
+      value: "",
       cardData: [],
       renderStockCard: false
     };
 
-    this.handleFormSubmit = this.handleFormSubmit.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
     this.handleChange = this.handleChange.bind(this);
   }
 
-  handleFormSubmit(value) {
+  handleSubmit(event) {
+    let value = this.state.value;
     alpha.data.quote(value).then(data => {
       const polished = alpha.util.polish(data);
       console.log(polished);
-      this.setState({
-        cardData: polished,
-        searchTerm: ""
-      });
+      this.setState({ cardData: polished });
     });
+    event.preventDefault();
   }
 
   handleChange(event) {
-    this.setState({value: event.target.value});
+    this.setState({ value: event.target.value });
   }
 
   render() {
     return (
-      // <div style={{ marginRight: "65%", marginLeft: "2.5%" }}>
-      //   <Search
-      //     placeholder="input search text"
-      //     onSearch={value => this.handleFormSubmit(value)}
-      //   />
-      //   {/* <StockCard /> */}
-      // </div>
-
-      <form>
-        <label>Symbol:</label>
-        <input type="text" value={this.state.searchTerm} onChange={this.handleChange} />
-      </form>
+      <div>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Name:
+            <input
+              type="text"
+              value={this.state.value}
+              onChange={this.handleChange}
+            />
+          </label>
+          <input type="submit" value="Submit" />
+        </form>
+      </div>
     );
   }
 }
--- client/src/Components/StockCard.js
+++ client/src/Components/StockCard.js
@@ -1,11 +1,19 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React, { Component } from "react";
+import { connect } from "react-redux";
 
 class StockCard extends Component {
+  constructor(props) {
+    super(props);
+  }
 
   render() {
-    return();
+    return (
+      <div>
+        <h1>{this.props.data.symbol}</h1>
+        <h1>{this.props.data.price}</h1>
+      </div>
+    );
   }
 }
 
-export default (StockCard);
+export default StockCard;
